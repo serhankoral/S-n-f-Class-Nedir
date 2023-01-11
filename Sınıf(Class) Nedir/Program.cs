e44a98677e5ea2723912bfbc3ed564319e8eef81
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-11 22:22:55 +0300

    Proje dosyası ekle.

---
+++ Sınıf(Class) Nedir/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sınıf_Class__Nedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
